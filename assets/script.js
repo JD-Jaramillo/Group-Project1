a8bc4a1400aca1a3623f347008d674de359baee4
JD-Jaramillo <jaramillo784@gmail.com>
2021-02-25 22:38:18 -0500

    Added footer button to HTML, centered and styled the items on CSS and started fetching the API

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,19 @@
-'https://api.edamam.com/searchq=chicken&app_id4e225b66&app_key=686ff1f044d786ecc6a84c188451b037'
-h 
+var appKey = '686ff1f044d786ecc6a84c188451b037';
+var searchInput = '';
+
+var edamamUrl = 'https://api.edamam.com/searchq=' + searchInput + '&app_id4e225b66&app_key=' + appKey;
+
+
+fucntion edamamUrl() {
+    fetch()
+}
+
+
+
+
+
+
+
+
+
+
