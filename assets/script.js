870af184d147d9717e04a6f331056b841f5bfc25
John Jaramillo <jaramillo784@gmail.com>
2021-02-27 21:59:39 -0500

    got the third API working

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,6 @@
-var cardWrapperBlock = document.querySelector('.card-wrapper-block');
-var drinkChoice = 'gin';
+var recipeCardWrapperBlock = document.querySelector('.card-wrapper-block');
+var drinksCardWrapperBlock = document.querySelector('.card__wrapper-block');
 
-// function demo() {
-//     var cards = document.querySelectorAll('.card');
-//     console.log('cards', cards);
-
-//     for (var i=0; i < cards.length; i++) {
-//         cards[i].
-//     }
-// }
-// demo();
 
 function edamamUrl(recipeInput) {
     console.log(" recipe function working");
@@ -53,19 +44,18 @@ function edamamUrl(recipeInput) {
                 ${recipeIngr}
                 </div>
                 </div></div>`
-                cardWrapperBlock.appendChild(recipeCardWrapper);
+                recipeCardWrapperBlock.appendChild(recipeCardWrapper);
             });
-
-            cocktailDBUrl();
 
         })
 };
 
 function clearCards() {
-    cardWrapperBlock.innerHTML = ' ';
+    recipeCardWrapperBlock.innerHTML = ' ';
+    drinksCardWrapperBlock.innerHTML = ' ';
 }
 
-function cocktailDBUrl() {
+function cocktailDBUrl(drinkChoice) {
     console.log("cocktail function working");
     var cocktailDBUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + drinkChoice;
 
@@ -75,55 +65,115 @@ function cocktailDBUrl() {
         })
         .then(function (data) {
             console.log("cocktail API", cocktailDBUrl);
-            var drinkName1 = data.drinks[0].strDrink;
-            console.log('name', drinkName1);
-            var drinkName1 = data.drinks[1].strDrink;
-            console.log('name', drinkName1);
-            var drinkName1 = data.drinks[2].strDrink;
-            console.log('name', drinkName1);
-            var drinkName1 = data.drinks[3].strDrink;
-            console.log('name', drinkName1);
-            var drinkName1 = data.drinks[4].strDrink;
-            console.log('name', drinkName1);
-            var drinkName1 = data.drinks[5].strDrink;
-            console.log('name', drinkName1);
-            var drinkImage1 = data.drinks[0].strDrinkThumb;
-            console.log('image', drinkImage1);
-            var drinkImage2 = data.drinks[1].strDrinkThumb;
-            console.log('image', drinkImage2);
-            var drinkImage3 = data.drinks[2].strDrinkThumb;
-            console.log('image', drinkImage3);
-            var drinkImage4 = data.drinks[3].strDrinkThumb;
-            console.log('image', drinkImage4);
-            var drinkImage5 = data.drinks[4].strDrinkThumb;
-            console.log('image', drinkImage5);
-            var drinkImage6 = data.drinks[5].strDrinkThumb;
-            console.log('ID', drinkImage6);
-            var drinkId1 = parseInt(data.drinks[0].idDrink);
-            console.log('ID', drinkId1);
-            var drinkId2 = parseInt(data.drinks[1].idDrink);
-            console.log('ID', drinkId2);
-            var drinkId3 = parseInt(data.drinks[2].idDrink);
-            console.log('ID', drinkId3);
-            var drinkId4 = parseInt(data.drinks[3].idDrink);
-            console.log('ID', drinkId4);
-            var drinkId5 = parseInt(data.drinks[4].idDrink);
-            console.log('ID', drinkId5);
-            var drinkId6 = parseInt(data.drinks[5].idDrink);
-            console.log('ID', drinkId6);
 
+
+            for (var i = 0; i < 6; i++) {
+
+                var drinksApiArray1 = data.drinks[i];
+
+                console.log("drinksApiArray1 for each ", drinksApiArray1);
+
+                var drinkName = drinksApiArray1.strDrink;
+                var drinkImage = drinksApiArray1.strDrinkThumb;
+                var drinkId = drinksApiArray1.idDrink;
+
+                var drinksCardWrapper = document.createElement("a");
+                drinksCardWrapper.classList.add('card__link-tag');
+                drinksCardWrapper.target = "_blank";
+                drinksCardWrapper.innerHTML =
+                    `<div class="card"> <div class="card-image">
+                <figure class="image is-4by3">
+                <img src="${drinkImage}" alt="Placeholder image">
+                </figure>
+                </div>
+                <div class="card-content">
+                <div class="media">
+                <div class="media-content">
+                <p class="title is-4">${drinkName}</p>
+                </div>
+                </div>
+
+                <div id="id-${i}" class="content drink__ingr-content">
+                ${drinkId}
+                </div>
+                </div></div>`
+                drinksCardWrapperBlock.appendChild(drinksCardWrapper);
+
+                fetchDrinkIngredients(drinkId, i);
+
+            }
+        });
+}
+
+function fetchDrinkIngredients(drinkId, i) {
+    var drinkIngrUrl = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + drinkId;
+
+    fetch(drinkIngrUrl)
+        .then(function (response) {
+            return response.json();
         })
-};
+        .then(function (data) {
+            console.log(data);
+            console.log('ingr1', data.drinks[0].strIngredient1);
+            console.log('ing2', data.drinks[0].strIngredient2);
+            console.log('ing3', data.drinks[0].strIngredient3);
+            console.log('ing4', data.drinks[0].strIngredient4);
+
+            var ingr1 = data.drinks[0].strIngredient1;
+            var ingr2 = data.drinks[0].strIngredient2;
+            var ingr3 = data.drinks[0].strIngredient3;
+            if (data.drinks[0].strIngredient4 !== null && data.drinks[0].strIngredient4 !== undefined) {
+                console.log('this this is not null')
+                var ingr4 = data.drinks[0].strIngredient4;
+            } else {
+                console.log('this thing is null');
+            }
+
+            console.log('i', i);
+
+            var drinkWrapper = document.getElementById('id-' + i);
+            console.log(drinkWrapper, 'drinkWrapper');
+            drinkWrapper.innerHTML =
+                `<span>${ingr1} </span><br> <span>${ingr2} </span><br> <span>${ingr3} </span><br> <span>${ingr4} </span>`
+
+            // var drinkIngrWrappers = document.querySelectorAll('#id-${i}');
+            // for (var i=0; i<6; i++) {
+            //     drinkIngrWrappers[i].innerHTML = 
+            // }
+        })
+}
 
 $("#recipeBtn").on("click", function () {
     clearCards();
-    // capture the value in citySearched and trim it to get rid of white spaces
     var recipeInput = $('#search_input').val().trim();
 
-    // IF value of cityInput is not equal to "" then create an Li element, add a classs, add the city input and append it to the ul
     if (recipeInput !== "") {
         console.log('recipe button working');
         edamamUrl(recipeInput);
     }
 })
+
+$('.drink__select').change(function (e) {
+    console.log('e', e);
+    $drink = $(".drink__select option:selected").val();
+    console.log('$drink', $drink);
+    // console.log('$drink', typeOf($drink));
+    cocktailDBUrl($drink);
+});
+// $('.selector').on("click", function (event) {
+//     clearCards();
+//     console.log('selector click working', event);
+//     function drinkFinder(choice) {
+//         console.log(choice, 'drink choice');
+//         var drinkDropdown = document.querySelectorAll(".drink__choices");
+//         console.log(drinkDropdown, 'dropdown function working');
+
+
+//         var drinkChoice = event.target.innerText;
+//         cocktailDBUrl(drinkChoice);
+//     }
+// })
+
+// drinkFinder();
+
 
